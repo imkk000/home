68298fc69694f4d56bd31c7b1939cd9dc882ce86
NB <nattakit.boonyang@gmail.com>
2025-05-07 00:03:36 +0700

    Update config

--- install.fish
+++ install.fish
@@ -4,6 +4,11 @@ function make_link -a path out_path
   end
 end
 
+# make symbolic link
 make_link git/gitconfig ~/.gitconfig
 make_link git/gitignore ~/.gitignore
 make_link fish/config.fish ~/.config/fish/config.fish
+
+# set go
+go telemetry off
+go env -w GOPATH=$HOME/.go
