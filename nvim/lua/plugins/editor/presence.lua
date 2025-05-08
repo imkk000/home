5733d819d52bfb36fa0bef5255a5f5106cfa5e58
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2025-05-09 03:59:24 +0700

    Remove unused plugins

--- nvim/lua/plugins/editor/presence.lua
+++
@@ -1,8 +0,0 @@
--- Plugin: https://github.com/andweeb/presence.nvim
-return {
-  plugins.editor.repo_presence,
-  name = plugins.editor.presence,
-  event = "VeryLazy",
-  config = true,
-  enabled = false,
-}
