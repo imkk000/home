ba5c35cca8415dd5981a40e4bd9d8d9dcaf24330
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2025-05-09 22:07:19 +0700

    Load default friendly snippet

--- nvim/lua/plugins/coding/cmp.lua
+++ nvim/lua/plugins/coding/cmp.lua
@@ -62,7 +62,8 @@ return {
     local ls = require("luasnip")
     local lspkind = require("lspkind")
 
-    require("luasnip.loaders.from_vscode").lazy_load({
+    require("luasnip.loaders.from_vscode").lazy_load()
+    require("luasnip.loaders.from_vscode").load({
       paths = { "~/.config/nvim/snippets" },
     })
 
