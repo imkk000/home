3ec89c5d7afd9152ac11bd9a4cd1adfa0207765e
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2025-05-14 19:21:05 +0700

    Add flash remote

--- nvim/lua/plugins/editor/flash.lua
+++ nvim/lua/plugins/editor/flash.lua
@@ -5,7 +5,8 @@ return {
   event = "VeryLazy",
   -- stylua: ignore
   keys = {
-    { "s", mode = { "n", "o", "x" }, function() require("flash").jump() end, desc = "Flash" },
+    { "s", mode = { "n", "o", "x" }, function() require("flash").jump() end,   desc = "Flash" },
+    { "r", mode = "o",               function() require("flash").remote() end, desc = "Flash Remote" },
   },
   opts = {
     modes = {
