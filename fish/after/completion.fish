a76dcf0dcda98e659b88132a8c84b0eb61575a70
NB <nattakit.boonyang@gmail.com>
2025-06-18 20:20:10 +0700

    Update configs

--- fish/after/completion.fish
+++ fish/after/completion.fish
@@ -5,6 +5,8 @@ fzf_configure_bindings --history=\cr --directory=\cf --git_status=\cg --git_log=
 
 switch (uname)
     case Darwin
+        rbenv init - fish | source
+
         if [ -f "$HOME/.google-cloud-sdk/path.fish.inc" ]
             . "$HOME/.google-cloud-sdk/path.fish.inc"
         end
